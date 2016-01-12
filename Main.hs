0d8abd817c332ea093ca48b782fa89eea041c6ca
Jeff K <jak19@sfu.ca>
2016-01-11 19:00:33 -0800

    temporarily moving zlib and binary back to main

--- Main.hs
+++ Main.hs
@@ -1,6 +1,15 @@
 import System.IO
 import System.Directory
 
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString as B
+
+import Codec.Compression.Zlib.Raw
+import Data.Binary.Get
+import Data.Word
+import Data.Int
+import Data.Bits
+
 import System.Environment (getArgs)
 
 import Control.Applicative
