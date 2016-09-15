f7896d3b22f90b1fdfdec6e4ec453d801a714954
Jeff <jeff@x-0.ca>
2016-09-15 00:47:08 +0000

    Cigar gets its own module

---
+++ src/Bio/Data/Cigar.hs
@@ -0,0 +1,13 @@
+module Cigar (Cigar, readcig) where
+
+data Cigar = Cigar {op_len::Int, op::Char}
+
+instance Show Cigar where
+    show s = (show . op_len $ s) ++ [(op s)]
+
+readcig :: Word32 -> Cigar 
+readcig s =
+    let op_len = fromIntegral $ s `shiftR` 4
+        op = fromIntegral $ 7 .&. s
+        t = "MIDNSHP=X" in
+    Cigar op_len (t!!op)
